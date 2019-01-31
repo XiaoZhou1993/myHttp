88e82c0bbe320bc4d86b0a330561b42df9f5bd32
XiaoZhou1993 <915303570@qq.com>
2019-01-31 22:24:05 +0800

    fix some bugs

--- server/src/myHttpMsg.cpp
+++ server/src/myHttpMsg.cpp
@@ -23,27 +23,31 @@ myHttpMessage::myHttpMessage(char* httpMsg)
 	const char* delimPara2 = "?";
 
 	tempString = strtok(httpMsg, delimPara1);
-	if(!tempString)
-		exit(0);
 	m_method = tempString;
+	if(true == m_method.empty())
+		exit(0);
 	HTTP_LOG_INFO("m_method = %s", m_method.c_str());
 
 	tempString = strtok(NULL, delimPara2);
-	if(!tempString)
-		exit(0);
 	m_url = tempString;
+ 	if(true == m_url.empty())
+		exit(0);
 	HTTP_LOG_INFO("m_url = %s", m_url.c_str());
 
 	tempString = strtok(NULL, delimPara1);
-	if(!tempString)
-		exit(0);
 	m_path = tempString;
+	if(true == m_path.empty())
+		exit(0);
 	HTTP_LOG_INFO("m_path = %s", m_path.c_str());
 }
 
 myHttpMessage::~myHttpMessage()
 {
 	HTTP_LOG_INFO("Enter the ~myHttpMessage!");
+
+	m_method.clear();
+	m_url.clear();
+	m_path.clear();
 }
 
 std::string myHttpMessage::getHttpMethod()
@@ -53,5 +57,6 @@ std::string myHttpMessage::getHttpMethod()
 
 bool myHttpMessage::judgeHttpReqType(const std::string &aimType)
 {
+	HTTP_LOG_INFO("Enter the judgeHttpReqType !");
 	return m_method == aimType;
 }
